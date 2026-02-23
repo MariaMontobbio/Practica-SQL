b6d3e08df874e8120d6c5a02e3f67b1214525d31
“Anna <anna.egea@gracia.lasalle.cat>
2026-02-23 12:42:18 +0100

    Creació de taula (Persona) / Anna

--- ex1.sql
+++ ex1.sql
@@ -1,2 +1,12 @@
 --Anna Egea, Xavi Merino i Maria Montobbio
 
+CREATE TABLE IF NOT EXISTS Persona (
+    DNI INT,
+    Nom VARCHAR(20),
+    Cognom1 VARCHAR(20),
+    Cognom2 VARCHAR(20),
+    Nacionalitat VARCHAR(20),
+    Sexe VARCHAR(20),
+    DataNaixament DATE,
+    PRIMARY KEY (DNI)
+);
